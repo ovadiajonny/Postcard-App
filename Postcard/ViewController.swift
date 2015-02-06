b88177e89b31f0f510014b691374aa4530faa8b3
Jonathan Ovadia <JonathanOvadia@Jonathans-iMac.local>
2015-02-06 21:27:51 +0200

    first rev of project

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var messageTextField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendButtonPressed(sender: UIButton) {
+        messageLabel.hidden = false;
+        var height: Int = 10
+        for (var i: Int = 1; i<height; i++) {
+            textView.text = textView.text +   "|"+spaces(i,rows: height) + multiplyString(String(i), count: i) + spaces(i,rows: height) + "| \n"
+        
+            
+        }
+        messageTextField.resignFirstResponder()
+      messageLabel.text = messageTextField.text
+        
+        messageLabel.textColor = UIColor(red: CGFloat(drand48()), green: CGFloat(drand48()), blue: CGFloat(drand48()), alpha: 1.0)
+        
+    }
+    
+    func spaces(row: Int,rows: Int) -> String{
+       var spaces: Int = rows-row
+        
+        return multiplyString(" ", count: spaces)
+    }
+    
+    func multiplyString (s: String, count: Int) -> String{
+    var newString = ""
+        
+        for (var i:Int = 0; i < count;i++){
+            newString = newString + s
+        }
+        return newString
+    }
 
 }
-
+ 
